e66b8c4273f1d89a5160b47f2090415682a7a826
steneor <ernesto@free.fr>
2021-10-09 18:32:56 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'raspi-myconfig'
+copyright = '2021, steneor'
+author = 'steneor'
 
 release = '0.1'
 version = '0.1.0'
